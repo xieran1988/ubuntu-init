c6bfa59815c8f7ea82074fdbe483a9de3fa31649
Nomi Xie <cfanfrank@gmail.com>
2012-02-25 19:56:30 +0800

    beauti

--- init.sh
+++ init.sh
@@ -2,19 +2,18 @@
 
 #sudo passwd
 #sudo cp sudoders /etc/
+
 #mkdir .ssh
 #cp id_rsa .ssh/  
 #chmod 600 .ssh/id_rsa
 
 apt=http://mirrors.163.com/ubuntu
-sudo -s <<E
-#cd /etc/apt/
-#cp sources.list sources.list.bak
-#sed -i "s,http://[^/].*/ubuntu,$apt,g" sources.list
-#apt-get update
-#apt-get install -y vim
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo sed -i "s,http://[^/].*/ubuntu,$apt,g" /etc/apt/sources.list
+sudo apt-get update
+
+#sudo apt-get install -y vim
 #cat .vimrc >> /etc/vim/vimrc
-E
 
 #sudo apt-get install bcmwl-kernel-source
 #sudo modprobe -r b43 ssb wl
